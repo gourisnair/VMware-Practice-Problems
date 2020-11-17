3d172dc8d7010abfafb5c0f6dcee0a4f9d08b466
Gouri S Nair <39928477+gourisnair@users.noreply.github.com>
2020-11-17 18:23:35 +0530

    Create ParanthesisBalancing_Using_Stack.cpp

---
+++ Coding/ParanthesisBalancing_Using_Stack.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isBalanced(string s) {
+    stack<char>st;
+    char tmp;
+    int l = s.length();
+    for(int i = 0; i < l; i++) {
+        if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            st.push(s[i]);
+            continue;
+        }
+        if(s.empty()) {
+            return false;
+        }
+        switch(s[i]) {
+            case ')': tmp = st.top();
+            st.pop();
+            if(tmp == '{' || tmp == '[') {
+                return false;
+            }
+            break;
+            case '}': tmp = st.top();
+            st.pop();
+            if(tmp == '(' || tmp == '[') {
+                return false;
+            }
+            break;
+            case ']': tmp = st.top();
+            st.pop();
+            if(tmp == '(' || tmp == '{') {
+                return false;
+            }
+            break;
+        }
+    }
+    return(st.empty());
+}
+int main() {
+    string s;
+    int t;
+    cin >> t;
+    while(t--) {
+        cin >> s;
+        if(isBalanced(s)) {
+            cout << "Balanced" << endl;
+        } else {
+            cout << "Not Balanced" << endl;
+        }
+    }
+    return 0;
+}
