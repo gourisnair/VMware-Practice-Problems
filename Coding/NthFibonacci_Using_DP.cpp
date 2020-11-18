b3f65f91740d42b47695a0775ced01c048cf2f95
Gouri S Nair <39928477+gourisnair@users.noreply.github.com>
2020-11-18 21:02:06 +0530

    Update and rename Fibonacci_Using_DP.cpp to NthFibonacci_Using_DP.cpp

---
+++ Coding/NthFibonacci_Using_DP.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define m 1000000007 //for bigger Ns, the value can be really big. So to avoid that, it was asked in the question to take modulu with 1000000007
+
+
+// User function Template for C++
+class Solution {
+  public:
+    long long int nthFibonacci(long long int n){
+        vector<long long int>dparr(n+2, 0);
+        dparr[1] = 1;
+        dparr[2] = 1;
+        for(int i = 3; i <= n; i++) {
+            dparr[i] = (dparr[i-1]%m+dparr[i-2]%m)%m;
+        }
+        return dparr[n];
+    }
+};
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        long long int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.nthFibonaccy(n) <<endl;
+    }
+    return 0;
+}
